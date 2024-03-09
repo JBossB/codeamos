4afec7320e046c7aad71185fdbb7b715de997ed3
Jonathan Baeza <eng.jbaeza@gmail.com>
2024-03-09 00:47:41 -0300

    node-js hola mundo

---
+++ node-js/01-hola-mundo/app3.js
@@ -0,0 +1,19 @@
+console.log('Inicio de programa')
+
+setTimeout(() => {
+    console.log('Primer Time out')
+}, 3000)
+
+
+setTimeout(() => {
+    console.log('Segundo Time out')
+}, 0)
+
+
+setTimeout(() => {
+    console.log('Tercer Time out')
+}, 2000)
+
+
+
+console.log('Fin de programa')
